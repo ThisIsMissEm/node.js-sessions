18873ee87d875d666ee3bf98f8907f69a4615cbb
Micheil Smith <micheil@yettobebranded.net>
2009-12-07 00:18:29 +1100

    Reworked the SessionManager Cleanup, still needs a little more work, but it's functional.

--- Sessions.js
+++ Sessions.js
@@ -14,6 +14,37 @@
     	}
 	}
 	
+	function pad(value, len) {
+		var len = len || 2;
+		var value = new String(value);
+
+		if(value.length < len){
+			while(value.length < len){
+				value = "0"+value;
+			}
+		}
+		return value;
+	}
+	
+	function timestamp(time){
+		var time = time ? new Date(time) : new Date;
+		return pad(time.getUTCDate())+"/"+pad(time.getMonth())+"/"+time.getFullYear()+" "+pad(time.getHours())+":"+pad(time.getMinutes())+":"+pad(time.getSeconds())+":"+pad(time.getMilliseconds(), 4);
+	}
+	
+	sys.log = function(){
+		sys.puts("\033[0;37m"+timestamp()+"\033[0m\t"+Array.prototype.join.call(arguments, " ")+"\033[0m ");
+	};
+	
+	function size(obj){
+		var len = 0;
+		for(var x in obj){
+			if(Object.hasOwnProperty.call(obj, x)){
+				++len;
+			}
+		}
+		return len;
+	}
+	
 	// The Session Manager:
 
 	var SessionManager = function(options){
@@ -36,35 +67,33 @@
 		var _sid = this.generateId();
 		var session = {
 			sid: _sid,
-			expires: (+new Date) + this.lifetime*1000,
+			expires: Math.floor((+new Date) + this.lifetime*1000),
 			data: {}
 		};
-	
 		
 		this._sessionStore[_sid] = session;
-		
 		this.emit("create", _sid);
 		
-		debug(this._sessionStore, true, "");
+		sys.log("\033[0;32m+++ "+_sid+"\tExpires: "+timestamp(session.expires));
 		
-		if(this._cleaner === null){
+		if(!this.timer){
 			this.cleanup();
 		}
-		
+				
 		return this._sessionStore[_sid];
 	};
-
+	
+	
+	
 	SessionManager.prototype.changeSession = function(sid, data){
 		this._sessionStore[sid].data = data;
-		this._sessionStore[sid].expires = (+new Date) + this.lifetime*1000;
-	
+		this._sessionStore[sid].expires = Math.floor((+new Date) + this.lifetime*1000);
 		this.emit("change", sid, data);
 	};
 	
 	SessionManager.prototype.deleteSession = function(sid){
 		this.emit("destroy", sid);
-		
-		delete this._sessionStore[sid];
+		//delete this._sessionStore[sid];
 	};
 	
 	SessionManager.prototype.generateId = function(){
@@ -79,48 +108,68 @@
 	
 	SessionManager.prototype.cleanup = function(){
 		var sessionExpiration,
-			 now = (+new Date),
+			 now = Date.now(),
 			 next = Infinity;
 		
-		sys.puts(now + "\tCleanup Running.");
+		sys.log(">>> Cleaning Up.");
+		sys.puts(sys.inspect(this._sessionStore));
+		sys.log("\033[0;33m=== Sessions:"+ size(this._sessionStore));
 		
 		for(var sid in this._sessionStore){
 			if(Object.prototype.hasOwnProperty.call(this._sessionStore, sid)){
-		
+				
 				sessionExpiration = this._sessionStore[sid].expires;
-				if(sessionExpiration < now){
-					this.deleteSession(sid);
+				
+				sys.log(">>> Checking: "+sid+": "+(sessionExpiration-now));
+				
+				// Using a Max Difference because timers can be delayed by a few milliseconds.
+				if(sessionExpiration - now < 100){
+					sys.log("\033[0;31m--- "+sid);
+					delete this._sessionStore[sid];
 				} else {
-					next = sessionExpiration < next ? sessionExpiration-now : next;
+					next = next > sessionExpiration ? sessionExpiration : next;
 				}
 			}
 		}
-	
-		if(next < Infinity){
-			sys.puts("Cleanup Scheduled!");
-			this._cleaner = setTimeout(this.cleanup, next);
+		
+		sys.log("\033[0;33m=== Sessions:"+ size(this._sessionStore));
+		
+		if(next < Infinity && next >= 0){
+			sys.log(">>> Next Cleanup at: "+timestamp(next));
+			
+			var self = this;
+			this.timer = setTimeout(function(){
+				self.cleanup.apply(self, []);
+			}, next-now);
+			
 		} else {
-			sys.puts("No More Cleanups Needed!");
+			delete this.timer;
+			sys.log(">>> No More Cleanups. ");
+			sys.puts(sys.inspect(this._sessionStore));
 		}
-		debug(this._sessionStore, true, "");
 	};
-
+	
+	var defaultSessionManager = new SessionManager();
+	
 	var Session = function(manager){
-		this.session = manager.createSession();
-		this.manager = manager;
+		this.manager = manager || defaultSessionManager;
+		this.session = this.manager.createSession.apply(this.manager, []);
 	};
 
 	Session.prototype.destroy = function(){};
 	Session.prototype.lookup = function(req){};
 	Session.prototype.lookupOrCreate = function(req){};
 	
-	Session.prototype.getHeader = function(){
-		var parts = ['SID=' + this.session.sid];
+	Session.prototype.getHeader = function(key){
+		var key = key || "SID";
+		
+		var parts = [key+'=' + this.session.sid];
+		
 		if(this.manager.path){
-			parts.push('path=' + this.path)
+			parts.push('path=' + this.manager.path)
 		}
 		if(this.manager.domain){
-			parts.push('domain=' + this.domain)
+			parts.push('domain=' + this.manager.domain)
 		}
 		if(this.manager.persistent){
 			function pad(n) {
@@ -136,7 +185,9 @@
 	
 		return parts.join('; ');
 	};
-
+	
+	
+	
 
 	exports.create = Session;
 	exports.manager = SessionManager;
